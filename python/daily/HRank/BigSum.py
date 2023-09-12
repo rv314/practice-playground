ec1008273b4bcb7325f83279db01bcba168d10cb
rv314 <ravi.v.pai@gmail.com>
2023-09-12 11:26:37 -0400

    sum of elements in array

---
+++ python/daily/HRank/BigSum.py
@@ -0,0 +1,30 @@
+'''
+In this challenge, you are required to calculate and print the sum of the elements in an array, 
+keeping in mind that some of those integers may be quite large.
+
+Function Description
+
+Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements.
+
+'''
+
+
+def aVeryBigSum(ar):
+    # Write your code here
+    num = 0
+    for i in ar:
+        num += i
+    return num
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
