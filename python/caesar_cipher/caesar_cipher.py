3ba5274ca862dc343f55d1f7dc99eb2dc91438e6
rv314 <rvp314@gmail.com>
2023-09-16 10:48:49 -0400

    cipher prog

--- python/caesar_cipher/caesar_cipher.py
+++ python/caesar_cipher/caesar_cipher.py
@@ -1,3 +1,4 @@
+# letter list
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
