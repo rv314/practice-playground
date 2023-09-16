1eddecdbb3bfcca759a95fb292396d43c28c8839
rv314 <rvp314@gmail.com>
2023-09-16 10:47:33 -0400

    compute bill

--- python/daily/HRank/BonAppetit.py
+++ python/daily/HRank/BonAppetit.py
@@ -4,14 +4,10 @@ import random
 import re
 import sys
 
-#
-# Complete the 'bonAppetit' function below.
-#
 # The function accepts following parameters:
 #  1. INTEGER_ARRAY bill
 #  2. INTEGER k
 #  3. INTEGER b
-#
 
 def bonAppetit(bill, k, b):
     
